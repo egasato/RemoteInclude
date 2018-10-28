fc737ae9d0cfaf5bd98308d406334cf4565bd0b7
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-10-28 11:07:03 +0100

    Add simple test file

---
+++ test/Test.cmake
@@ -0,0 +1,9 @@
+#######################################################################
+# Copyright (c) 2018 Esaú García Sánchez-Torija                       #
+#                                                                     #
+# This Source Code Form is subject to the terms of the Mozilla Public #
+# License, v. 2.0. If a copy of the MPL was not distributed with this #
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.            #
+#######################################################################
+
+message(STATUS "It Works!")
