3f1d49ba3cb276d4a493922cd5d080c5a4fe3213
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-11-10 19:29:03 +0100

    Add missing properties

--- project.properties.sh
+++ project.properties.sh
@@ -88,3 +88,18 @@ unset var
 [[ "$_PROJECT_URL"          == "" ]] && _PROJECT_URL="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME"
 [[ "$_PROJECT_VCS"          == "" ]] && _PROJECT_VCS="$_PROJECT_URL.git"
 [[ "$_PROJECT_SOURCE"       == "" ]] && _PROJECT_SOURCE="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME/releases/download/$_PROJECT_NAME.tar.gz"
+[[ "$_LABEL_BUILD_DATE"     == "" ]] && _LABEL_BUILD_DATE="$(date +%FT%T.%NZ)"
+[[ "$_LABEL_NAME"           == "" ]] && _LABEL_NAME="$_PROJECT_NAME"
+[[ "$_LABEL_DESCRIPTION"    == "" ]] && _LABEL_DESCRIPTION="$_PROJECT_DESCRIPTION"
+[[ "$_LABEL_USAGE"          == "" ]] && _LABEL_USAGE="$_PROJECT_URL/blob/v$_PROJECT_VERSION/README.md"
+[[ "$_LABEL_URL"            == "" ]] && _LABEL_URL="$_PROJECT_HOMEPAGE"
+[[ "$_LABEL_VCS_URL"        == "" ]] && _LABEL_VCS_URL="$_PROJECT_VCS"
+[[ "$_LABEL_VENDOR"         == "" ]] && _LABEL_VENDOR="$_PROJECT_VENDOR"
+[[ "$_LABEL_VERSION"        == "" ]] && _LABEL_VERSION="v$_PROJECT_VERSION"
+if [[ "$_LABEL_VCS_REF" == "" ]]; then
+    _LABEL_VCS_REF=$(git describe --exact-match --tags HEAD 2> /dev/null)
+    if [[ $? -ne 0 ]]; then
+        _LABEL_VCS_REF=$(git rev-parse HEAD)
+        _LABEL_USAGE="$_PROJECT_URL/blob/$_LABEL_VCS_REF/README.md"
+    fi
+fi
