224e875286e0a76841e6957898867b27176dc7a6
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-11-16 21:54:37 +0100

    Add CMakeLists version extractor

---
+++ scripts/get_version_CMakeLists.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get the absolute path of the script
+if [[ "${BASH_SOURCE[0]}" == "" ]]; then
+    __filename=$(cd "$(dirname "$0")" && pwd -P)/$(basename "$0")
+else
+    __filename=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/$(basename "${BASH_SOURCE[0]}")
+fi
+__dirname=$(dirname "$__filename")
+
+# Returns the version number used inside CMakeLists.txt
+function get_version_CMakeLists() {
+    gawk -f "$__dirname/get_version_CMakeLists.gawk" CMakeLists.txt
+}
