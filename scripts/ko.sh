31b5236a3da6c0fdb8c31796a660a3940cb9dfbc
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-11-16 21:56:54 +0100

    Add OK and KO printers

---
+++ scripts/ko.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Prints a red KO
+function ko() {
+    echo -e "\e[41mKO\e[49m"
+}
---
+++ scripts/ok.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Prints a green OK
+function ok() {
+    echo -e "\e[32mOK\e[39m"
+}
