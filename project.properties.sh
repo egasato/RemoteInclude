c52754b7831c6a8a2bd0ee42ce951cf3f248b2f0
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-11-17 19:33:36 +0100

    Fix source url, error output from sha512sum command and add dummy sha512

--- project.properties.sh
+++ project.properties.sh
@@ -50,8 +50,8 @@ unset var
 [[ "$_PROJECT_HOMEPAGE"     == "" ]] && _PROJECT_HOMEPAGE="https://$_AUTHOR_USER.github.io/$_PROJECT_NAME"
 [[ "$_PROJECT_URL"          == "" ]] && _PROJECT_URL="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME"
 [[ "$_PROJECT_VCS"          == "" ]] && _PROJECT_VCS="$_PROJECT_URL.git"
-[[ "$_PROJECT_SOURCE"       == "" ]] && _PROJECT_SOURCE="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME/releases/download/$_PROJECT_NAME.tar.gz"
-[[ "$_PROJECT_SHA512"       == "" ]] && _PROJECT_SHA512=$(sha512sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" | cut -d' ' -f1)
+[[ "$_PROJECT_SOURCE"       == "" ]] && _PROJECT_SOURCE="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME/releases/download/v$_PROJECT_VERSION/$_PROJECT_NAME.tar.gz"
+[[ "$_PROJECT_SHA512"       == "" ]] && _PROJECT_SHA512=$(sha512sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
 [[ "$_LABEL_BUILD_DATE"     == "" ]] && _LABEL_BUILD_DATE="$(date +%FT%T.%NZ)"
 [[ "$_LABEL_NAME"           == "" ]] && _LABEL_NAME="$_PROJECT_NAME"
 [[ "$_LABEL_DESCRIPTION"    == "" ]] && _LABEL_DESCRIPTION="$_PROJECT_DESCRIPTION"
@@ -60,10 +60,15 @@ unset var
 [[ "$_LABEL_VCS_URL"        == "" ]] && _LABEL_VCS_URL="$_PROJECT_VCS"
 [[ "$_LABEL_VENDOR"         == "" ]] && _LABEL_VENDOR="$_PROJECT_VENDOR"
 [[ "$_LABEL_VERSION"        == "" ]] && _LABEL_VERSION="v$_PROJECT_VERSION"
+
+# Fix variables
 if [[ "$_LABEL_VCS_REF" == "" ]]; then
     _LABEL_VCS_REF=$(git describe --exact-match --tags HEAD 2> /dev/null)
     if [[ $? -ne 0 ]]; then
         _LABEL_VCS_REF=$(git rev-parse HEAD)
         _LABEL_USAGE="$_PROJECT_URL/blob/$_LABEL_VCS_REF/README.md"
     fi
+fi
+if [[ "$_PROJECT_SHA512" == "" ]]; then
+    _PROJECT_SHA512="00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 fi
