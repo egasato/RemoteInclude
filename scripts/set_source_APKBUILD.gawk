d825f2f7eae77fe7a7f7307355cc07eaa5ecf37d
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-12-14 17:11:29 +0100

    Fix source for (A)PK(G)BUILD

--- scripts/set_source_APKBUILD.gawk
+++ scripts/set_source_APKBUILD.gawk
@@ -1,9 +1,9 @@
 /^source=.*$/ {
     contains = match($0, /^(source=").*(".*)$/, arr)
     if (contains > 0) {
-        gsub(project_name,    "${pkgname}", project_source)
+        gsub(project_name, "${_pkgname}",   project_source)
         gsub(project_version, "${pkgver}",  project_source)
-        print arr[1] "${pkgname}-${pkgver}.tar.gz::" project_source arr[2]
+        print arr[1] "${_pkgname}-${pkgver}.tar.gz::" project_source arr[2]
         next
     }
 }
--- scripts/set_source_APKBUILD.sh
+++ scripts/set_source_APKBUILD.sh
@@ -9,6 +9,6 @@ function set_source_APKBUILD() {
         __filename=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/$(basename "${BASH_SOURCE[0]}")
     fi
     local __dirname=$(dirname "$__filename")
-    gawk -v project_source="$1" -v project_name="$2" -v project_version="$3" -v project_name="$2" -f "$__dirname/set_source_APKBUILD.gawk" "$__dirname/../APKBUILD" > "$__dirname/../APKBUILD.tmp"
+    gawk -v project_source="$1" -v project_name="$2" -v project_version="$3" -f "$__dirname/set_source_APKBUILD.gawk" "$__dirname/../APKBUILD" > "$__dirname/../APKBUILD.tmp"
     mv "$__dirname/../APKBUILD.tmp" "$__dirname/../APKBUILD"
 }
