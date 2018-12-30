ff6a3fa080b94a87e419c1a5f6bcda10ee1c6353
Esaú García Sánchez-Torija <egasato@protonmail.com>
2018-12-30 18:25:41 +0100

    Add more project properties and change format

--- project.properties.sh
+++ project.properties.sh
@@ -42,29 +42,45 @@ done
 unset _ULTRA_PRIVATE
 unset var
 
+# This shit shouldn't exist because I'm basically hard-coding the variables names
+# and the only reason this script exists is to avoid that, but I'm tired of seeing
+# warnings and errors in my IDE, so here we go
+declare CMAKE_BUILD_TYPE
+declare _AUTHOR_USER _AUTHOR_NAME _AUTHOR_NAME_ASCII _AUTHOR_EMAIL _PROJECT_NAME _PROJECT_SUMMARY _PROJECT_DESCRIPTION _PROJECT_VERSION _PROJECT_LICENSE
+declare _AUTHOR_CONTACT _AUTHOR_CONTACT_ASCII
+declare _PROJECT_NAME_LOWER _PROJECT_LICENSE_PKG _PROJECT_LICENSE_APK _PROJECT_VENDOR _PROJECT_VENDOR_ASCII _PROJECT_HOMEPAGE _PROJECT_URL _PROJECT_VCS _PROJECT_SOURCE _PROJECT_MD5 _PROJECT_SHA1 _PROJECT_SHA224 _PROJECT_SHA256 _PROJECT_SHA384 _PROJECT_SHA512
+declare _LABEL_BUILD_DATE _LABEL_NAME _LABEL_DESCRIPTION _LABEL_USAGE _LABEL_URL _LABEL_VCS_URL _LABEL_VENDOR _LABEL_VERSION _LABEL_VCS_REF
+
 ## Set our variables
 [[ "$_AUTHOR_CONTACT"       == "" ]] && _AUTHOR_CONTACT="$_AUTHOR_NAME <$_AUTHOR_EMAIL>"
 [[ "$_AUTHOR_CONTACT_ASCII" == "" ]] && _AUTHOR_CONTACT_ASCII="$_AUTHOR_NAME_ASCII <$_AUTHOR_EMAIL>"
+
+[[ "$_PROJECT_NAME_LOWER"   == "" ]] && _PROJECT_NAME_LOWER=$(echo "$_PROJECT_NAME" | tr 'A-Z' 'a-z' | tr -s ' ' | tr ' ' '-')
+[[ "$_PROJECT_LICENSE_PKG"  == "" ]] && _PROJECT_LICENSE_PKG=$(echo "$_PROJECT_LICENSE" | sed -r 's/(v|\.0)//g')
+[[ "$_PROJECT_LICENSE_APK"  == "" ]] && _PROJECT_LICENSE_APK=$(echo "$_PROJECT_LICENSE" | sed -r 's/\.0//g')
 [[ "$_PROJECT_VENDOR"       == "" ]] && _PROJECT_VENDOR="$_AUTHOR_NAME (Open Source Developer)"
 [[ "$_PROJECT_VENDOR_ASCII" == "" ]] && _PROJECT_VENDOR_ASCII="$_AUTHOR_NAME_ASCII (Open Source Developer)"
-[[ "$_PROJECT_HOMEPAGE"     == "" ]] && _PROJECT_HOMEPAGE="https://$_AUTHOR_USER.github.io/$_PROJECT_NAME"
-[[ "$_PROJECT_URL"          == "" ]] && _PROJECT_URL="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME"
-[[ "$_PROJECT_VCS"          == "" ]] && _PROJECT_VCS="$_PROJECT_URL.git"
-[[ "$_PROJECT_SOURCE"       == "" ]] && _PROJECT_SOURCE="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME/releases/download/v$_PROJECT_VERSION/$_PROJECT_NAME.tar.gz"
-[[ "$_PROJECT_MD5"          == "" ]] && _PROJECT_MD5=$(md5sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_PROJECT_SHA1"         == "" ]] && _PROJECT_SHA1=$(sha1sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_PROJECT_SHA224"       == "" ]] && _PROJECT_SHA224=$(sha224sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_PROJECT_SHA256"       == "" ]] && _PROJECT_SHA256=$(sha256sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_PROJECT_SHA384"       == "" ]] && _PROJECT_SHA384=$(sha384sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_PROJECT_SHA512"       == "" ]] && _PROJECT_SHA512=$(sha512sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
-[[ "$_LABEL_BUILD_DATE"     == "" ]] && _LABEL_BUILD_DATE="$(date +%FT%T.%NZ)"
-[[ "$_LABEL_NAME"           == "" ]] && _LABEL_NAME="$_PROJECT_NAME"
-[[ "$_LABEL_DESCRIPTION"    == "" ]] && _LABEL_DESCRIPTION="$_PROJECT_DESCRIPTION"
-[[ "$_LABEL_USAGE"          == "" ]] && _LABEL_USAGE="$_PROJECT_URL/blob/v$_PROJECT_VERSION/README.md"
-[[ "$_LABEL_URL"            == "" ]] && _LABEL_URL="$_PROJECT_HOMEPAGE"
-[[ "$_LABEL_VCS_URL"        == "" ]] && _LABEL_VCS_URL="$_PROJECT_VCS"
-[[ "$_LABEL_VENDOR"         == "" ]] && _LABEL_VENDOR="$_PROJECT_VENDOR"
-[[ "$_LABEL_VERSION"        == "" ]] && _LABEL_VERSION="v$_PROJECT_VERSION"
+[[ "$_PROJECT_HOMEPAGE"       == "" ]] && _PROJECT_HOMEPAGE="https://$_AUTHOR_USER.github.io/$_PROJECT_NAME"
+[[ "$_PROJECT_URL"            == "" ]] && _PROJECT_URL="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME"
+[[ "$_PROJECT_VCS"            == "" ]] && _PROJECT_VCS="$_PROJECT_URL.git"
+[[ "$_PROJECT_SOURCE"         == "" ]] && _PROJECT_SOURCE="https://github.com/$_AUTHOR_USER/$_PROJECT_NAME/releases/download/v$_PROJECT_VERSION/$_PROJECT_NAME.tar.gz"
+[[ "$_PROJECT_MD5"            == "" ]] && _PROJECT_MD5=$(md5sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+[[ "$_PROJECT_SHA1"           == "" ]] && _PROJECT_SHA1=$(sha1sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+[[ "$_PROJECT_SHA224"         == "" ]] && _PROJECT_SHA224=$(sha224sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+[[ "$_PROJECT_SHA256"         == "" ]] && _PROJECT_SHA256=$(sha256sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+[[ "$_PROJECT_SHA384"         == "" ]] && _PROJECT_SHA384=$(sha384sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+[[ "$_PROJECT_SHA512"         == "" ]] && _PROJECT_SHA512=$(sha512sum "build/${CMAKE_BUILD_TYPE:-Release}/$_PROJECT_NAME.tar.gz" 2> /dev/null | cut -d' ' -f1)
+
+[[ "$_LABEL_BUILD_DATE"  == "" ]] && _LABEL_BUILD_DATE="$(date +%FT%T.%NZ)"
+[[ "$_LABEL_NAME"        == "" ]] && _LABEL_NAME="$_PROJECT_NAME"
+[[ "$_LABEL_DESCRIPTION" == "" ]] && _LABEL_DESCRIPTION="$_PROJECT_SUMMARY"
+[[ "$_LABEL_USAGE"       == "" ]] && _LABEL_USAGE="$_PROJECT_URL/blob/v$_PROJECT_VERSION/README.md"
+[[ "$_LABEL_URL"         == "" ]] && _LABEL_URL="$_PROJECT_HOMEPAGE"
+[[ "$_LABEL_VCS_URL"     == "" ]] && _LABEL_VCS_URL="$_PROJECT_VCS"
+[[ "$_LABEL_VENDOR"      == "" ]] && _LABEL_VENDOR="$_PROJECT_VENDOR"
+[[ "$_LABEL_VERSION"     == "" ]] && _LABEL_VERSION="v$_PROJECT_VERSION"
+
+[[ "$_CMAKE_BUILD_TYPE"  == "" ]] && _CMAKE_BUILD_TYPE="Release"
 
 # Fix variables
 if [[ "$_LABEL_VCS_REF" == "" ]]; then
